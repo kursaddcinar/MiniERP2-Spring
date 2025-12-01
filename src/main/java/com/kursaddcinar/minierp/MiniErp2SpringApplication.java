8a592272a1d696035b1a74a297cf2f95a85a4b3a
kursaddcinar <kursaddcinar@gmail.com>
2025-12-01 23:35:55 +0300

    Initialize project with Spring Boot dependencies

---
+++ src/main/java/com/kursaddcinar/minierp/MiniErp2SpringApplication.java
@@ -0,0 +1,13 @@
+package com.kursaddcinar.minierp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniErp2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniErp2SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kursaddcinar/minierp/MiniErp2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kursaddcinar.minierp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniErp2SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
