eaca7822fa2443e406f3ef268c7769a5c1967ff9
kursaddcinar <kursaddcinar@gmail.com>
2025-12-04 22:09:56 +0300

    Add User entities and update pom.xml

---
+++ src/main/java/com/kursaddcinar/minierp/entity/Role.java
@@ -0,0 +1,32 @@
+package com.kursaddcinar.minierp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+public class Role extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @Column(name = "role_name", length = 50, nullable = false, unique = true)
+    private String roleName;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "is_active")
+    private boolean isActive = true;
+
+    @OneToMany(mappedBy = "role")
+    private List<UserRole> userRoles = new ArrayList<>();
+}
---
+++ src/main/java/com/kursaddcinar/minierp/entity/UserRole.java
@@ -0,0 +1,32 @@
+package com.kursaddcinar.minierp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_roles")
+@Getter
+@Setter
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_role_id")
+    private Integer userRoleId;
+
+    @CreationTimestamp
+    @Column(name = "created_date", updatable = false)
+    private LocalDateTime createdDate;
+
+    @ManyToOne(fetch = FetchType.EAGER) // Rol bilgisi her zaman lazım
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+}
